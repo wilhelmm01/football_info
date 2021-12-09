f04f6ec1ca5c6b65e5811c861313927b89102686
TomFoolery <christophertravisp91@gmail.com>
2021-12-08 21:04:02 -0500

    actual api

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,8 @@
-const cfbd = require('cfb.js');
-
-const defaultClient = cfb.ApiClient.instance;
-
-const ApiKeyAuth = defaultClient.authentications['ApiKeyAuth'];
-ApiKeyAuth.apiKey = "hfcyfCh3nFnMrE6H7t8BexqchMRuSZsQ/kiTA/1cuRhQLxnA5ZYJk2X8QnJmtRwf";
-ApiKeyAuth.apiKeyPrefix["Authorization"] = "Bearer";
-
-var api = new cfb.GamesApi();
-
-let year = 2021;
-const games = await api.getGames(year);
-
-// // base URL
-
-// api.collegefootballdata.com/
-
 //weather API - 
-ApiKeyAuth.apiKey = "aef20bc076b6576a68a8168ed03dc6dc";
+// ApiKeyAuth.apiKey = "aef20bc076b6576a68a8168ed03dc6dc";
+// Ticketmaster apiKey = "aFemIeE1x3rB7wbi2X9ArZEygXHkEBuT"
+
+
+    fetch("https://app.ticketmaster.com/discovery/v2/venues.json?size=100?latlong&apikey=aFemIeE1x3rB7wbi2X9ArZEygXHkEBuT")
+    .then(resp => resp.json())
+    .then( json => console.log(json))
