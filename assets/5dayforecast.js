fc2554be4a04d5ffbbd92bea135a19cfbae8f081
Logan Davis <Ldavis96@live.com>
2021-12-13 19:18:08 -0500

    monday commit

---
+++ assets/5dayforecast.js
@@ -0,0 +1,43 @@
+var getFiveDayForecast = (event) => {
+    let city = $('#search-city').val();
+    // Set up URL for API search using forecast search
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    // Fetch from API
+    fetch(queryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            // HTML template
+            let fiveDayForecastHTML = `
+        <h2>5-Day Forecast:</h2>
+        <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
+            // Loop over the 5 day forecast and build the template HTML using UTC offset and Open Weather Map icon
+            for (let i = 0; i < response.list.length; i++) {
+                let dayData = response.list[i];
+                let dayTimeUTC = dayData.dt;
+                let timeZoneOffset = response.city.timezone;
+                let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+                let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+                let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+                // Only displaying mid-day forecasts
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                    fiveDayForecastHTML += `
+                <div class="weather-card card m-2 p0">
+                    <ul class="list-unstyled p-3">
+                        <li>${thisMoment.format("MM/DD/YY")}</li>
+                        <li class="weather-icon"><img src="${iconURL}"></li>
+                        <li>Temp: ${dayData.main.temp}&#8457;</li>
+                        <br>
+                        <li>Humidity: ${dayData.main.humidity}%</li>
+                    </ul>
+                </div>`;
+                }
+            }
+            // Build the HTML template
+            fiveDayForecastHTML += `</div>`;
+            // Append the five-day forecast to the DOM
+            $('#five-day-forecast').html(fiveDayForecastHTML);
+        })
+}
--- assets/weather.js
+++ assets/weather.js
@@ -3,26 +3,52 @@ var gameForecast = document.getElementById("upcoming");
 var weatherForecast = document.getElementById("forecast");
 var currentEvent = document.getElementById("currentEvent")
 
-//Retreiving Weather function
+//Variables for local storage
+const cityList = document.querySelector("#cityList");
+var cities = JSON.parse(localStorage.getItem("cities")) || [];
+//Logan adding local storage for cities, creates list of each previously
+cities.forEach(city => {
+        const li = document.createElement('li');
+        li.innerText = city;
+        cityList.appendChild(li);
+    })
+    //Retreiving Weather function
 function getWeatherApi() {
 
     //Retreiving city from clicking button
     document.getElementById("city-btn").onclick = function(city) {
-    var city = document.getElementById("city").value; 
-    console.log(city)
-        
+        var city = document.getElementById("city").value;
+        console.log(city)
 
-        
-    //Current Forecast
-    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
-     
-    console.log("API Link: " + requestURL)  
-    
-    fetch(requestURL)
-        .then(function (response) {
-            return response.json();
+        //Logan adding local storage for cities
+        if (cities.indexOf(city) === -1) {
+            cities.push(city);
+        }
+
+        localStorage.setItem('cities', JSON.stringify(cities));
+        //Logan adding line to clear data after one search, appends current search to box on page
+        cityList.innerHTML = ""
+        cities.forEach(city => {
+            const li = document.createElement('li');
+            li.innerText = city;
+            cityList.appendChild(li);
         })
-        .then (function(data){
+
+
+
+
+        //Current Forecast
+        var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
+
+        console.log("API Link: " + requestURL)
+            //Logan adding line to clear data after one search
+        gameForecast.innerHTML = "";
+
+        fetch(requestURL)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
 
                 //Console Logging for testing
                 //console.log("Current Temp " + data.main.temp + "°F" );
@@ -30,44 +56,44 @@ function getWeatherApi() {
                 //console.log("High Temp " + data.main.temp_max + "°F");
                 //console.log("Low Temp " + data.main.temp_min + "°F");
                 //console.log ("Wind Speed " +data.wind.speed + " MPH")
-                console.log ("dt " + data.dt);
-            
-                
+                console.log("dt " + data.dt);
+
+
 
                 //Date info
                 var dateResponse = (data.dt);
                 var date = new Date(0);
                 date.setUTCSeconds(dateResponse);
                 var d = date;
-                console.log("date " +d);
+                console.log("date " + d);
                 var dOutput = d.getFullYear();
-                console.log(" date updated" +dOutput);
-                var monthOutput = d.getMonth()+1;
+                console.log(" date updated" + dOutput);
+                var monthOutput = d.getMonth() + 1;
                 console.log(monthOutput);
                 var dayOutput = d.getDate();
                 console.log(dayOutput);
 
 
-                
+
                 //Creating the elements for display
 
-               var currentDate = document.createElement('h2');
-                currentDate.textContent= monthOutput + "/" + dayOutput + "/" + dOutput;
+                var currentDate = document.createElement('h2');
+                currentDate.textContent = monthOutput + "/" + dayOutput + "/" + dOutput;
 
-               var currentTemp = document.createElement('h3');
-               currentTemp.textContent = parseInt(data.main.temp)+ "° F" ;
+                var currentTemp = document.createElement('h3');
+                currentTemp.textContent = parseInt(data.main.temp) + "° F";
 
-               var feelsLike = document.createElement('h4');
-               feelsLike.textContent="Feels Like " + parseInt(data.main.feels_like) + "° F";
+                var feelsLike = document.createElement('h4');
+                feelsLike.textContent = "Feels Like " + parseInt(data.main.feels_like) + "° F";
 
-               var highTemp = document.createElement('p');
-               highTemp.textContent="H: " + parseInt(data.main.temp_max) + "° F";
+                var highTemp = document.createElement('p');
+                highTemp.textContent = "H: " + parseInt(data.main.temp_max) + "° F";
 
-               var lowTemp = document.createElement('p');
-               lowTemp.textContent="L: " + parseInt(data.main.temp_min) + "° F";
+                var lowTemp = document.createElement('p');
+                lowTemp.textContent = "L: " + parseInt(data.main.temp_min) + "° F";
 
-               var windSpeed = document.createElement('p');
-               windSpeed.textContent="Wind: " + parseInt(data.wind.speed)  + " MPH";
+                var windSpeed = document.createElement('p');
+                windSpeed.textContent = "Wind: " + parseInt(data.wind.speed) + " MPH";
 
                 //var icons = document.createElement("img")
                 var conditions = data.weather[0].description;
@@ -75,77 +101,80 @@ function getWeatherApi() {
 
                 var lat = (data.coord.lat);
                 var lon = (data.coord.lon);
-                
+
                 console.log(lat);
                 console.log(lon);
 
 
 
-              gameForecast.appendChild(currentDate);
-              gameForecast.appendChild(currentTemp);
-              gameForecast.appendChild(feelsLike);
-              gameForecast.appendChild(windSpeed);
-              gameForecast.appendChild(highTemp);
-              gameForecast.appendChild(lowTemp);
-
-              
+                gameForecast.appendChild(currentDate);
+                gameForecast.appendChild(currentTemp);
+                gameForecast.appendChild(feelsLike);
+                gameForecast.appendChild(windSpeed);
+                gameForecast.appendChild(highTemp);
+                gameForecast.appendChild(lowTemp);
 
 
-              var requestURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&city=" + city + "&apikey=aFemIeE1x3rB7wbi2X9ArZEygXHkEBuT";
-                      // console.log("API Link: " + requestURL)  
-                      fetch(requestURL)
-                          .then(function (response) {
-                              return response.json();
-                          })
-                          .then (function(data){
-                                  
-                                  //Creating the elements for display
-                              var eventName = document.createElement('h3');
-                              eventName.textContent =(data._embedded.events[0].name);
-                              var startTime = document.createElement('h4');
-                              startTime.textContent=(data._embedded.events[0].dates.start.localTime);
-                              var date = document.createElement('p');
-                              date.textContent=(data._embedded.events[0].dates.start.localDate);
-                              currentEvent.appendChild(eventName);
-                              currentEvent.appendChild(startTime);
-                              currentEvent.appendChild(date);
-                              currentEvent.appendChild(time);
-                          });
-                      
-                  
-                  
-                        
-//5-day forecast        
-        var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + key;
-        fetch(forecastURL)
-        .then(function (response) {
-            return response.json();
-            
-        })
-        .then(function(response){
-            console.log( "dt" + response.daily[0].dt);
-
-            for (var i=1; i<8 ;i++){
-               var futureDateResponse = (response.daily[i].dt);
-               var fd = new Date(i);
-               fd.setUTCSeconds(futureDateResponse);
 
 
-               var fiveDay = document.createElement("h1");
-               fiveDay.textContent = fd;
+                var requestURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&city=" + city + "&apikey=aFemIeE1x3rB7wbi2X9ArZEygXHkEBuT";
+                // console.log("API Link: " + requestURL)  
+                fetch(requestURL)
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        //Logan adding line to clear data after one search
+                        currentEvent.innerHTML = "";
+                        //Creating the elements for display
+                        var eventName = document.createElement('h3');
+                        eventName.textContent = (data._embedded.events[0].name);
+                        var startTime = document.createElement('h4');
+                        startTime.textContent = (data._embedded.events[0].dates.start.localTime);
+                        var date = document.createElement('p');
+                        date.textContent = (data._embedded.events[0].dates.start.localDate);
+                        currentEvent.appendChild(eventName);
+                        currentEvent.appendChild(startTime);
+                        currentEvent.appendChild(date);
+                        currentEvent.appendChild(time);
+                    });
 
-                weatherForecast.appendChild(fiveDay);
 
-                console.log(fd);
-            }
 
-        })
-        });
-    
+
+                //5-day forecast        
+                var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + key;
+                fetch(forecastURL)
+                    .then(function(response) {
+                        return response.json();
+
+                    })
+                    .then(function(response) {
+                        console.log("dt" + response.daily[0].dt);
+                        //Logan adding line to clear data after one search
+                        weatherForecast.innerHTML = "";
+
+                        for (var i = 1; i < 8; i++) {
+                            var futureDateResponse = (response.daily[i].dt);
+                            var fd = new Date(i);
+                            fd.setUTCSeconds(futureDateResponse);
+
+
+                            var fiveDay = document.createElement("h1");
+                            fiveDay.textContent = fd;
+
+                            weatherForecast.appendChild(fiveDay);
+
+                            console.log(fd);
+                        }
+
+                    })
+            });
+
     }
- }     //5 day forecase
+} //5 day forecase
 
-    
+
 
 getWeatherApi();
 
