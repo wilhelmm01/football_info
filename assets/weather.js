e764c31bdcc6927cc99615d5ecd3cb34f5ac78a1
Logan Davis <Ldavis96@live.com>
2021-12-14 22:16:43 -0500

    fixed background color and responsive tiles

--- assets/weather.js
+++ assets/weather.js
@@ -7,7 +7,9 @@ var currentEvent = document.getElementById("currentEvent")
 const cityList = document.querySelector("#cityList");
 var cities = JSON.parse(localStorage.getItem("cities")) || [];
 //Logan adding local storage for cities, creates list of each previously
+
 cities.forEach(city => {
+
         const li = document.createElement('li');
         li.innerText = city;
         cityList.appendChild(li);
@@ -40,16 +42,15 @@ function getWeatherApi() {
         //Current Forecast
         var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
 
-        console.log("API Link: " + requestURL)
-            //Logan adding line to clear data after one search
-        gameForecast.innerHTML = "";
+        console.log("API Link: " + requestURL);
+        //Logan adding line to clear data after one search
 
         fetch(requestURL)
             .then(function(response) {
+                gameForecast.innerHTML = "";
                 return response.json();
             })
             .then(function(data) {
-
                 //Console Logging for testing
                 //console.log("Current Temp " + data.main.temp + "°F" );
                 //console.log("Feels Like " +data.main.feels_like + "°F");
@@ -57,6 +58,7 @@ function getWeatherApi() {
                 //console.log("Low Temp " + data.main.temp_min + "°F");
                 //console.log ("Wind Speed " +data.wind.speed + " MPH")
                 console.log("dt " + data.dt);
+
 
 
 
@@ -118,31 +120,31 @@ function getWeatherApi() {
 
 
                 var requestURL2 = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&sort=date,asc&city=" + city + "&apikey=aFemIeE1x3rB7wbi2X9ArZEygXHkEBuT";
-                      // console.log("API Link: " + requestURL)  
-                    fetch(requestURL2)
-                    
-                        .then(function (response) {
-                            console.log(response)
-                            return response.json();
-                        })
-                        .then (function(data){
-                            var events = data._embedded.events;
-                            // console.log(data);
+                // console.log("API Link: " + requestURL)  
+                fetch(requestURL2)
 
-                            for (var i=0; i < events.length ; i++) {
+                .then(function(response) {
+                        console.log(response)
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        var events = data._embedded.events;
+                        // console.log(data);
+
+                        for (var i = 0; i < events.length; i++) {
 
                             //     var data = data.length
-                        
-                                  //Creating the elements for display
+
+                            //Creating the elements for display
                             var eventName = document.createElement('p');
-                            eventName.textContent =(data._embedded.events[i].name);
-                            
+                            eventName.textContent = (data._embedded.events[i].name);
+
                             var startTime = document.createElement('p');
-                            startTime.textContent=(data._embedded.events[i].dates.start.localTime);
-                            
+                            startTime.textContent = (data._embedded.events[i].dates.start.localTime);
+
                             var date = document.createElement('p');
-                            date.textContent=(data._embedded.events[i].dates.start.localDate);
-                            
+                            date.textContent = (data._embedded.events[i].dates.start.localDate);
+
                             currentEvent.appendChild(eventName);
                             currentEvent.appendChild(startTime);
                             currentEvent.appendChild(date);
@@ -170,7 +172,7 @@ function getWeatherApi() {
                             var forecastDate = new Date(i);
                             forecastDate.setUTCSeconds(futureDateResponse);
                             var futureDate = forecastDate;
-                            var forecastMonth = futureDate.getMonth()+1;
+                            var forecastMonth = futureDate.getMonth() + 1;
                             var forecastDay = futureDate.getDate();
                             var forecastYear = futureDate.getFullYear();
 
@@ -178,28 +180,29 @@ function getWeatherApi() {
 
                             var fiveDay = document.createElement("div");
                             fiveDay.classList.add("card")
-                            fiveDay.textContent=fdOutput
+                            fiveDay.textContent = fdOutput
                             weatherForecast.appendChild(fiveDay);
-                
-                 var highForecast=document.createElement("h3");
-               highForecast.textContent ="High Temp: " + parseInt(response.daily[i].temp.max)+ "° F" ;
 
-               var lowForecast = document.createElement("h3");
-               lowForecast.textContent ="Low Temp: " + parseInt(response.daily[i].temp.min)+ "° F";
+                            var highForecast = document.createElement("h3");
+                            highForecast.textContent = "High Temp: " + parseInt(response.daily[i].temp.max) + "° F";
 
-               var forecastDescription = document.createElement("p");
-               forecastDescription.textContent = (response.daily[i].weather[0].description);
+                            var lowForecast = document.createElement("h3");
+                            lowForecast.textContent = "Low Temp: " + parseInt(response.daily[i].temp.min) + "° F";
+
+                            var forecastDescription = document.createElement("p");
+                            forecastDescription.textContent = (response.daily[i].weather[0].description);
 
 
-                fiveDay.appendChild(highForecast);
-                fiveDay.appendChild(lowForecast);
-                fiveDay.appendChild(forecastDescription);
-                console.log(fdOutput);
-                console.log( "High temp " + response.daily[i].temp.max)
-                console.log("low temp " +response.daily[i].temp.min );
-                console.log("description " + response.daily[i].weather[0].description)
 
-                            
+                            fiveDay.appendChild(highForecast);
+                            fiveDay.appendChild(lowForecast);
+                            fiveDay.appendChild(forecastDescription);
+                            console.log(fdOutput);
+                            console.log("High temp " + response.daily[i].temp.max)
+                            console.log("low temp " + response.daily[i].temp.min);
+                            console.log("description " + response.daily[i].weather[0].description)
+
+
                         }
 
                     })
@@ -227,13 +230,13 @@ function getWeatherApi() {
 //             //Creating the elements for display
 //     var eventName = document.createElement('p');
 //     eventName.textContent =(data._embedded.events[i].name);
-    
+
 //     var startTime = document.createElement('p');
 //     startTime.textContent=(data._embedded.events[i].dates.start.localTime);
-    
+
 //     var date = document.createElement('p');
 //     date.textContent=(data._embedded.events[i].dates.start.localDate);
-    
+
 //     currentEvent.appendChild(eventName);
 //     currentEvent.appendChild(startTime);
 //     currentEvent.appendChild(date);
